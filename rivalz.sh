69bffd5d10a4eb4cb6d1743f4594aade37ad4b0d
londrwus <78261745+londrwus@users.noreply.github.com>
2024-08-27 19:47:35 +0300

    Update rivalz.sh

--- rivalz.sh
+++ rivalz.sh
@@ -1,1 +1,137 @@
-sudo sed -i 's/$/1/' /var/lib/dbus/machine-id
+channel_logo() {
+  echo -e '\033[0;31m'
+  echo -e '┌┐ ┌─┐┌─┐┌─┐┌┬┐┬┬ ┬  ┌─┐┬ ┬┌┐ ┬┬  '
+  echo -e '├┴┐│ ││ ┬├─┤ │ │└┬┘  └─┐└┬┘├┴┐││  '
+  echo -e '└─┘└─┘└─┘┴ ┴ ┴ ┴ ┴   └─┘ ┴ └─┘┴┴─┘'
+  echo -e '\e[0m'
+  echo -e "\n\nПодпишись на самый 4ekHyTbIu* канал в крипте @bogatiy_sybil [💸]"
+}
+
+find_file_path() {
+  local search_path="$1"
+  find "$search_path" -type f -name "filesystem.js" 2>/dev/null | grep "systeminformation/lib/filesystem.js" | head -n 1
+}
+
+while true; do
+    channel_logo
+    sleep 2
+    echo -e "\n\nМеню:"
+    echo "1. Установить ноду Rivalz"
+	echo "2. Обновить ноду"
+    echo "3. Поменять кошелек"
+    echo "4. Поменять потребляемое кол-во места диска"
+    echo "5. Информация о ноде"
+    echo -e "6. Выйти из скрипта\n"
+    read -p "Выберите пункт меню: " choice
+    
+    case $choice in
+      1)
+        echo "Начинаю установку ноды..."
+
+        # Upgrade dependencies
+        echo "Происходит обновление пакетов..."
+        if sudo apt update && sudo apt upgrade -y; then
+            echo "Обновление пакетов: Успешно"
+        else
+            echo "Обновление пакетов: Ошибка"
+            exit 1
+        fi
+
+        # Screen libary
+        echo "Установка screen..."
+        if sudo apt-get install screen; then
+            echo "Установка screen: Успешно"
+        else
+            echo "Установка screen: Ошибка"
+            exit 1
+        fi
+
+        # Install node.js
+        echo "Скачиваем Node.Js..."
+        curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+        if sudo apt install -y nodejs; then
+          echo "Устанвока Node.Js: Успешно"
+        else
+          echo "Установка Node.Js: Ошибка"
+          exit 1
+        fi
+
+        # Установка ноды Rivalz
+        log_message 'Установка ноды Rivalz...'
+        npm i -g rivalz-node-cli
+
+        # Define the file path
+        FILE="/usr/lib/node_modules/rivalz-node-cli/node_modules/systeminformation/lib/filesystem.js"
+
+        # Check if the file exists
+        if [ ! -f "$FILE" ]; then
+          echo "File not found at $FILE. Attempting to locate it..."
+          FILE=$(find_file_path "/usr/lib")
+
+          if [ -z "$FILE" ]; then
+            FILE=$(find_file_path "/usr/local/lib")
+          fi
+
+          if [ -z "$FILE" ]; then
+            FILE=$(find_file_path "/opt")
+          fi
+
+          if [ -z "$FILE" ]; then
+            # Adding check for ~/.nvm path
+            FILE=$(find_file_path "$HOME/.nvm")
+          fi
+
+          if [ -z "$FILE" ]; then
+            echo "Error: filesystem.js not found. Make sure npm is installed and the file path is correct."
+            exit 1
+          fi
+
+          echo "File found at $FILE"
+        fi
+
+        # Create a temporary file
+        TMP_FILE=$(mktemp)
+
+        # Define the original line and the replacement line
+        ORIGINAL_LINE="devices = outJSON.blockdevices.filter(item => { return (item.type === 'disk') && item.size > 0 && (item.model !== null || (item.mountpoint === null && item.label === null && item.fstype === null && item.parttype === null && item.path && item.path.indexOf('/ram') !== 0 && item.path.indexOf('/loop') !== 0 && item['disc-max'] && item['disc-max'] !== 0)); });"
+        NEW_LINE="devices = outJSON.blockdevices.filter(item => { return (item.type === 'disk') && item.size > 0 }).sort((a, b) => b.size - a.size);"
+
+        # Read through the file line by line
+        while IFS= read -r line
+        do
+          if [[ "$line" == *"$ORIGINAL_LINE"* ]]; then
+            echo "$NEW_LINE" >> "$TMP_FILE"
+          else
+            echo "$line" >> "$TMP_FILE"
+          fi
+        done < "$FILE"
+
+        # Replace the original file with the modified one
+        mv "$TMP_FILE" "$FILE"
+        ;;
+	    2)
+	      echo "Начинаю обновление ноды..."
+	      if rivalz update-version; then
+            echo "Обновление ноды: Успешно"
+        else
+            echo "Обновление ноды: Ошибка"
+            exit 1
+        fi
+        ;;
+      3)
+        rivalz change-wallet
+        ;;
+      4)
+        rivalz change-hardware-config
+        ;;
+      5)
+        rivalz info
+        ;;
+      6)
+        exit 0
+        ;;
+      *)
+        echo "Неверная пункт. Пожалуйста, выберите правильную цифру в меню."
+        ;;
+    esac
+  done
