f6b040877573c1dfe1f827f4f9e8dd475f050539
londrwus <78261745+londrwus@users.noreply.github.com>
2024-10-04 18:07:03 +0100

    Update story.sh

--- story.sh
+++ story.sh
@@ -8,7 +8,52 @@ channel_logo() {
 }
 
 download_node() {
-  echo 'Hello'
+  echo 'Начинаю установку...'
+
+  sudo apt-get update -y && sudo apt upgrade -y && sudo apt-get install make build-essential unzip lz4 gcc git jq -y
+
+  sudo rm -rf /usr/local/go
+  curl -Ls https://go.dev/dl/go1.21.1.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+  eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
+  eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
+
+  read -p "Придумайте название для своей ноды: " node_name
+  NODE_MONIKER="\"$node_name\""
+  echo "NODE_MONIKER=$NODE_MONIKER"
+
+  git clone https://github.com/piplabs/story
+  cd story
+  git checkout v0.10.2
+  go build -o story ./client
+  cp ./story /usr/local/bin/story
+  cd
+  story version
+  wget https://story-geth-binaries.s3.us-west-1.amazonaws.com/geth-public/geth-linux-amd64-0.9.3-b224fdf.tar.gz
+  tar -xvzf geth-linux-amd64-0.9.3-b224fdf.tar.gz
+  mv ./geth-linux-amd64-0.9.3-b224fdf/geth /usr/local/bin/geth
+  rm geth-linux-amd64-0.9.3-b224fdf.tar.gz
+
+  story init --moniker $NODE_MONIKER --network iliad
+
+  sudo tee /etc/systemd/system/story-geth.service > /dev/null <<EOF
+[Unit]
+Description=Story Geth
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which geth) --iliad --syncmode full --http --http.api eth,net,web3,engine --http.vhosts '*' --http.addr 0.0.0.0 --http.port 8545 --ws --ws.api eth,web3,net,txpool --ws.addr 0.0.0.0 --ws.port 8546
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+  sudo systemctl daemon-reload
+  sudo systemctl enable story-geth
+  sudo systemctl start story-geth  
 }
 
 exit_from_script() {
