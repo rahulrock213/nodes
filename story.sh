f770828ffe87c688ad033680d1462046fcc483f0
londrwus <78261745+londrwus@users.noreply.github.com>
2024-10-18 17:34:11 +0100

    Update story.sh

--- story.sh
+++ story.sh
@@ -10,39 +10,75 @@ channel_logo() {
 download_node() {
   echo 'Начинаю установку...'
 
-  sudo apt-get update -y && sudo apt upgrade -y && sudo apt-get install make build-essential unzip lz4 gcc git jq -y
+  sudo apt-get update -y && sudo apt upgrade -y && sudo apt-get install make build-essential screen unzip lz4 gcc git jq -y
 
+  screen -S storynode
+}
+
+keep_download() {
+  cd $HOME
+  VER="1.23.1"
+  wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
   sudo rm -rf /usr/local/go
-  curl -Ls https://go.dev/dl/go1.21.1.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
-  eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
-  eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
+  sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
+  rm "go$VER.linux-amd64.tar.gz"
+  [ ! -f ~/.bash_profile ] && touch ~/.bash_profile
+  echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
+  source $HOME/.bash_profile
+  [ ! -d ~/go/bin ] && mkdir -p ~/go/bin
 
-  read -p "Придумайте название для своей ноды: " node_name
-  NODE_MONIKER="\"$node_name\""
-  echo "NODE_MONIKER=$NODE_MONIKER"
+  read -p "Придумайте имя вашей ноде: " node_name
+  echo "export MONIKER=\"$node_name\"" >> $HOME/.bash_profile
+  echo "export STORY_CHAIN_ID="iliad-0"" >> $HOME/.bash_profile
+  echo "export STORY_PORT="52"" >> $HOME/.bash_profile
+  source $HOME/.bash_profile
 
+  cd $HOME
+  rm -rf bin
+  mkdir bin
+  cd bin
+  wget -O geth https://github.com/piplabs/story-geth/releases/download/v0.9.4/geth-linux-amd64
+  chmod +x geth
+  mv ~/bin/geth ~/go/bin/
+  mkdir -p ~/.story/story
+  mkdir -p ~/.story/geth
+
+  cd $HOME
+  rm -rf story
   git clone https://github.com/piplabs/story
   cd story
-  git checkout v0.10.2
+  git checkout v0.11.0
   go build -o story ./client
-  cp ./story /usr/local/bin/story
-  cd
-  story version
-  wget https://story-geth-binaries.s3.us-west-1.amazonaws.com/geth-public/geth-linux-amd64-0.9.3-b224fdf.tar.gz
-  tar -xvzf geth-linux-amd64-0.9.3-b224fdf.tar.gz
-  mv ./geth-linux-amd64-0.9.3-b224fdf/geth /usr/local/bin/geth
-  rm geth-linux-amd64-0.9.3-b224fdf.tar.gz
+  sudo mv ~/story/story ~/go/bin/
 
-  story init --moniker $NODE_MONIKER --network iliad
+  story init --moniker "$node_name" --network iliad
+
+  SEEDS="51ff395354c13fab493a03268249a74860b5f9cc@story-testnet-seed.itrocket.net:26656"
+  PEERS="2f372238bf86835e8ad68c0db12351833c40e8ad@story-testnet-peer.itrocket.net:26656,343507f6105c8ebced67765e6d5bf54bc2117371@38.242.234.33:26656,de6a4d04aab4e22abea41d3a4cf03f3261422da7@65.109.26.242:25556,7844c54e061b42b9ed629b82f800f2a0055b806d@37.27.131.251:26656,1d3a0e76b5cdf550e8a0351c9c8cd9b5285be8a2@77.237.241.33:26656,f1ec81f4963e78d06cf54f103cb6ca75e19ea831@217.76.159.104:26656,2027b0adffea21f09d28effa3c09403979b77572@198.178.224.25:26656,118f21ef834f02ab91e3fc3e537110efb4c1c0ac@74.118.140.190:26656,8876a2351818d73c73d97dcf53333e6b7a58c114@3.225.157.207:26656,caf88cbcd0628188999104f5ea6a5eed4a34422c@178.63.184.134:26656,7f72d44f3d448fd44485676795b5cb3b62bf5af0@142.132.135.125:20656"
+  sed -i -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*seeds *=.*/seeds = \"$SEEDS\"/}" \
+         -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*persistent_peers *=.*/persistent_peers = \"$PEERS\"/}" $HOME/.story/story/config/config.toml
+
+  wget -O $HOME/.story/story/config/genesis.json https://server-3.itrocket.net/testnet/story/genesis.json
+  wget -O $HOME/.story/story/config/addrbook.json  https://server-3.itrocket.net/testnet/story/addrbook.json
+
+  sed -i.bak -e "s%:1317%:${STORY_PORT}317%g;
+  s%:8551%:${STORY_PORT}551%g" $HOME/.story/story/config/story.toml
+  sed -i.bak -e "s%:26658%:${STORY_PORT}658%g;
+  s%:26657%:${STORY_PORT}657%g;
+  s%:26656%:${STORY_PORT}656%g;
+  s%^external_address = \"\"%external_address = \"$(wget -qO- eth0.me):${STORY_PORT}656\"%;
+  s%:26660%:${STORY_PORT}660%g" $HOME/.story/story/config/config.toml
+  sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.story/story/config/config.toml
+  sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.story/story/config/config.toml
 
   sudo tee /etc/systemd/system/story-geth.service > /dev/null <<EOF
 [Unit]
-Description=Story Geth
+Description=Story Geth daemon
 After=network-online.target
 
 [Service]
 User=$USER
-ExecStart=$(which geth) --iliad --syncmode full --http --http.api eth,net,web3,engine --http.vhosts '*' --http.addr 0.0.0.0 --http.port 8545 --ws --ws.api eth,web3,net,txpool --ws.addr 0.0.0.0 --ws.port 8546
+ExecStart=$(which geth) --iliad --syncmode full --http --http.api eth,net,web3,engine --http.vhosts '*' --http.addr 0.0.0.0 --http.port ${STORY_PORT}545 --authrpc.port ${STORY_PORT}551 --ws --ws.api eth,web3,net,txpool --ws.addr 0.0.0.0 --ws.port ${STORY_PORT}546
 Restart=on-failure
 RestartSec=3
 LimitNOFILE=65535
@@ -51,9 +87,49 @@ LimitNOFILE=65535
 WantedBy=multi-user.target
 EOF
 
-  sudo systemctl daemon-reload
-  sudo systemctl enable story-geth
-  sudo systemctl start story-geth  
+  sudo tee /etc/systemd/system/story.service > /dev/null <<EOF
+[Unit]
+Description=Story Service
+After=network.target
+
+[Service]
+User=$USER
+WorkingDirectory=$HOME/.story/story
+ExecStart=$(which story) run
+
+Restart=on-failure
+RestartSec=5
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target
+EOF
+
+  cp $HOME/.story/story/data/priv_validator_state.json $HOME/.story/story/priv_validator_state.json.backup
+
+  rm -rf $HOME/.story/story/data
+}
+
+final_download() {
+  screen -r storynode
+
+  mv $HOME/.story/story/priv_validator_state.json.backup $HOME/.story/story/data/priv_validator_state.json
+  rm -rf $HOME/.story/geth/iliad/geth/chaindata
+  mkdir -p $HOME/.story/geth/iliad/geth
+
+}
+
+export_wallet() {
+  cat /root/.story/story/config/private_key.txt
+
+  story validator export --export-evm-key
+}
+
+create_validator() {
+  story validator create --stake 1000000000000000000 --chain-id 1513 --private-key $(cat $HOME/.story/story/config/private_key.txt | grep "PRIVATE_KEY" | awk -F'=' '{print $2}')
+}
+
+check_logs() {
+  sudo journalctl -u story -f
 }
 
 exit_from_script() {
