407e404904203f248b4b6f664692e4a77b4c5296
londrwus <78261745+londrwus@users.noreply.github.com>
2024-10-22 17:02:16 +0100

    Update story.sh

--- story.sh
+++ story.sh
@@ -27,6 +27,8 @@ keep_download() {
   source $HOME/.bash_profile
   [ ! -d ~/go/bin ] && mkdir -p ~/go/bin
 
+  sudo rm -rf /usr/local/go
+  curl -Ls https://go.dev/dl/go1.23.1.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
   eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
   eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
 
