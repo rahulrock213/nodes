147bd60d081402f7b40fc871c77cb32fed9da600
londrwus <78261745+londrwus@users.noreply.github.com>
2024-11-26 14:11:24 +0000

    Update hemi.sh

--- hemi.sh
+++ hemi.sh
@@ -18,10 +18,10 @@ download_node() {
   eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
   eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
 
-  wget https://github.com/hemilabs/heminetwork/releases/download/v0.5.0/heminetwork_v0.5.0_linux_amd64.tar.gz
+  wget https://github.com/hemilabs/heminetwork/releases/download/v0.6.0/heminetwork_v0.6.0_linux_amd64.tar.gz
   mkdir -p hemi
-  tar --strip-components=1 -xzvf heminetwork_v0.5.0_linux_amd64.tar.gz -C hemi
-  sudo rm -rf heminetwork_v0.5.0_linux_amd64.tar.gz
+  tar --strip-components=1 -xzvf heminetwork_v0.6.0_linux_amd64.tar.gz -C hemi
+  sudo rm -rf heminetwork_v0.6.0_linux_amd64.tar.gz
 
   cd hemi/
 
@@ -38,7 +38,7 @@ download_node() {
   read -p "Как выполните, введите любую кнопку сюда: " checkjust
   
   echo "export POPM_PRIVATE_KEY=$private_key" >> ~/.bashrc
-  echo 'export POPM_STATIC_FEE=1040' >> ~/.bashrc
+  echo 'export POPM_STATIC_FEE=700' >> ~/.bashrc
   echo 'export POPM_BFG_URL=wss://testnet.rpc.hemi.network/v1/ws/public' >> ~/.bashrc
   source ~/.bashrc
 
@@ -51,7 +51,7 @@ After=network.target
 User=$USER
 Environment="POPM_BFG_REQUEST_TIMEOUT=60s"
 Environment="POPM_BTC_PRIVKEY=$private_key"
-Environment="POPM_STATIC_FEE=1040"
+Environment="POPM_STATIC_FEE=700"
 Environment="POPM_BFG_URL=wss://testnet.rpc.hemi.network/v1/ws/public"
 WorkingDirectory=$HOME/hemi
 ExecStart=$HOME/hemi/popmd
@@ -99,6 +99,144 @@ change_fee() {
   echo 'Значение FEE поменялось...'
 }
 
+auto_change() {
+  cd $HOME
+
+  sudo apt-get install screen jq pip -y
+  pip install bs4 -y
+  pip install requests -y
+
+  python_script="fetch_pop_txs.py"
+
+  service_file="/etc/systemd/system/hemid.service"
+  
+  prev_value_file="prev_pop_txs_value.txt"
+  
+  json_file="popm-address.json"
+  
+  public_key=$(jq -r '.public_key' "$json_file")
+  
+  cat <<EOF > "$python_script"
+import requests
+from bs4 import BeautifulSoup
+import os
+
+# URL of the page
+public_key = os.getenv('PUBLIC_KEY')
+url = f"https://testnet.popstats.hemi.network/pubkey/{public_key}.html"
+
+# Make a request to fetch the page content
+response = requests.get(url)
+
+# Check for 404 status code
+if response.status_code == 404:
+    print("404")
+else:
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    # Find all tables
+    tables = soup.find_all('table')
+
+    # Ensure there are at least 3 tables
+    if len(tables) >= 3:
+        third_table = tables[2]  # Select the third table
+        rows = third_table.find_all('tr')
+        pop_txs_value = rows[1].find_all('td')[0].text.strip()  # First cell in the second row
+        print(f"{pop_txs_value}")
+    else:
+        print("Error: The third table was not found on the page.")
+EOF
+  
+  update_fee() {
+      current_fee=$(grep -oP 'POPM_STATIC_FEE=\K\d+' "$service_file")
+      new_fee=$((current_fee + 500))
+      sed -i "s/POPM_STATIC_FEE=$current_fee/POPM_STATIC_FEE=$new_fee/" "$service_file"
+      echo "Updated POPM_STATIC_FEE to $new_fee"
+  }
+  
+  while true; do
+      if [ -f "$prev_value_file" ]; then
+          prev_value=$(cat "$prev_value_file")
+      else
+          prev_value=""
+      fi
+  
+      output=$(PUBLIC_KEY="$public_key" python3 "$python_script")
+      
+      if [ "$output" = "404" ]; then
+          current_fee=$(grep -oP 'POPM_STATIC_FEE=\K\d+' "$service_file")
+          if [ "$current_fee" -lt 1500 ]; then
+              sed -i "s/POPM_STATIC_FEE=$current_fee/POPM_STATIC_FEE=1500/" "$service_file"
+              sudo systemctl daemon-reload
+              sudo systemctl restart hemid
+              echo "Set POPM_STATIC_FEE to 1500 due to 404 error."
+          else
+              update_fee
+          fi
+      else
+          current_value=$(echo "$output" | grep -o '[0-9]\+')
+          if [ "$current_value" -eq "$prev_value" ]; then
+              update_fee
+          else
+              echo "$current_value" > "$prev_value_file"
+              echo "PoP Txs value has changed, no update to POPM_STATIC_FEE."
+          fi
+      fi
+  
+      # Sleep for 12 hours (43200 seconds)
+      sleep 43200
+  done
+}
+
+update_node() {
+  sudo systemctl stop hemid.service
+  sudo systemctl disable hemid.service
+  sudo rm /etc/systemd/system/hemid.service
+  sudo systemctl daemon-reload
+
+  cd $HOME
+  sudo rm -r hemi/
+
+  sudo apt-get update -y && sudo apt upgrade -y
+
+  wget https://github.com/hemilabs/heminetwork/releases/download/v0.6.0/heminetwork_v0.6.0_linux_amd64.tar.gz
+  mkdir -p hemi
+  tar --strip-components=1 -xzvf heminetwork_v0.6.0_linux_amd64.tar.gz -C hemi
+  sudo rm -rf heminetwork_v0.6.0_linux_amd64.tar.gz
+
+  cd hemi/
+
+  eth_address=$(jq -r '.ethereum_address' ~/popm-address.json)
+  private_key=$(jq -r '.private_key' ~/popm-address.json)
+  public_key=$(jq -r '.public_key' ~/popm-address.json)
+  pubkey_hash=$(jq -r '.pubkey_hash' ~/popm-address.json)
+
+  sudo tee /etc/systemd/system/hemid.service > /dev/null <<EOF
+[Unit]
+Description=Hemi
+After=network.target
+
+[Service]
+User=$USER
+Environment="POPM_BFG_REQUEST_TIMEOUT=60s"
+Environment="POPM_BTC_PRIVKEY=$private_key"
+Environment="POPM_STATIC_FEE=700"
+Environment="POPM_BFG_URL=wss://testnet.rpc.hemi.network/v1/ws/public"
+WorkingDirectory=$HOME/hemi
+ExecStart=$HOME/hemi/popmd
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+  sudo systemctl enable hemid
+  sudo systemctl daemon-reload
+  sudo systemctl start hemid
+}
+
 restart_node() {
   cd $HOME/hemi/
 
@@ -133,9 +271,11 @@ while true; do
     echo "2. 📜 Посмотреть логи"
     echo "3. 📊 Вывести данные"
     echo "4. 👨‍💻 Поменять значение POPM_STATIC_FEE"
-    echo "5. ♻️ Перезапустить ноду"
-    echo "6. 🗑️ Удалить ноду"
-    echo -e "7. 🚪 Выйти из скрипта\n"
+    echo "5. 🚴‍♂️ Скрипт по авто-обновление POPM_STATIC_FEE (BETA)"
+    echo "6. 🍪 Обновить ноду"
+    echo "7. ♻️ Перезапустить ноду"
+    echo "8. 🗑️ Удалить ноду"
+    echo -e "9. 🚪 Выйти из скрипта\n"
     read -p "Выберите пункт меню: " choice
 
     case $choice in
@@ -152,12 +292,18 @@ while true; do
         change_fee
         ;;
       5)
-        restart_node
+        auto_change
         ;;
       6)
-        delete_node
+        update_node
         ;;
       7)
+        restart_node
+        ;;
+      8)
+        delete_node
+        ;;
+      9)
         exit_from_script
         ;;
       *)
