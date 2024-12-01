0328092d1ddaba2f23838008d1fd0cd90269af05
londrwus <78261745+londrwus@users.noreply.github.com>
2024-12-01 16:00:15 +0000

    Update soneium.sh

--- soneium.sh
+++ soneium.sh
@@ -28,13 +28,15 @@ download_node() {
 
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io docker.io
+  sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io
   sudo usermod -aG docker $USER
   newgrp docker
 
   VER=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
   curl -L "https://github.com/docker/compose/releases/download/$VER/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
+
+  sudo apt-get install docker.io
 
 
   git clone https://github.com/Soneium/soneium-node.git
