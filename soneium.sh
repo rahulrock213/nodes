2ef576cf77d78fe12634186421bad36e8d73b8fd
londrwus <78261745+londrwus@users.noreply.github.com>
2024-12-01 07:13:16 +0000

    Update soneium.sh

--- soneium.sh
+++ soneium.sh
@@ -30,11 +30,11 @@ download_node() {
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io docker.io
-    sudo usermod -aG docker $USER
-    newgrp docker
   else
     echo "Docker уже установлен, пропускаем установку..."
   fi
+  sudo usermod -aG docker $USER
+  newgrp docker
 
   if ! command -v docker-compose &> /dev/null; then
     VER=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
