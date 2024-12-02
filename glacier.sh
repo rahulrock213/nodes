162dfa20f45b64f0c5991e14b5aee20246684df7
londrwus <78261745+londrwus@users.noreply.github.com>
2024-12-02 12:55:02 +0000

    Update glacier.sh

--- glacier.sh
+++ glacier.sh
@@ -30,7 +30,6 @@ download_node() {
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt update -y && sudo apt install -y docker-ce docker-ce-cli containerd.io
   sudo usermod -aG docker $USER
-  newgrp docker
   sudo apt install -y docker-buildx-plugin docker-compose-plugin
 
   sudo apt install docker.io -y
