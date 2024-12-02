5d8e3bf08e57eb012347aa3b337b627430610cef
londrwus <78261745+londrwus@users.noreply.github.com>
2024-12-02 09:23:17 +0000

    Update glacier.sh

--- glacier.sh
+++ glacier.sh
@@ -28,10 +28,12 @@ download_node() {
 
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo apt update -y && sudo apt install -y docker-ce docker-ce-cli containerd.io docker.io
+  sudo apt update -y && sudo apt install -y docker-ce docker-ce-cli containerd.io
   sudo usermod -aG docker $USER
   newgrp docker
   sudo apt install -y docker-buildx-plugin docker-compose-plugin
+
+  sudo apt install docker.io -y
 
   read -p "Введите ваш приватный ключ кошелька: " priv_key
   docker run -d -e PRIVATE_KEY=$priv_key --name glacier-verifier docker.io/glaciernetwork/glacier-verifier:v0.0.2
